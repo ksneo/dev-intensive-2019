4e7a62cfc6b86f555c836ef8ee3b2bd64fbb101a
Vladimir Kashapov <vkashapov@gmail.com>
2020-01-20 22:21:04 +0300

    Add missing files

---
+++ app/src/main/java/ru/skillbranch/devintensive/utils/Utils.kt
@@ -0,0 +1,17 @@
+package ru.skillbranch.devintensive.utils
+
+object Utils {
+    fun parseFullName(fullName: String?): Pair<String?, String?> {
+        return when (fullName) {
+            null -> (Pair(null, null))
+            "" -> (Pair(null, null))
+            else -> {
+                val parts: List<String>? = fullName?.split(" ")
+
+                val firstName = parts?.getOrNull(0)
+                val lastName = parts?.getOrNull(1)
+                return Pair(firstName, lastName)
+            }
+        }
+    }
+}
